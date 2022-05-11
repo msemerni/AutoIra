a7b7c3061ebacf272ad70c685482ce0b86162140
msemerni <you@example.com>
2022-05-12 02:47:23 +0300

    try add google mep copy

--- Zapolnyator/popup/pasteToJira.js
+++ Zapolnyator/popup/pasteToJira.js
@@ -1,12 +1,11 @@
 try {
+  window.focus();
   if (window.location.hostname.indexOf("placer.atlassian") === -1) {
     throw new Error("Switch to Jira ('https://placer.atlassian.net/...')");
   }
 
-  localStorage.removeItem("venueObject");
-
   const permission = navigator.permissions.query({ name: 'clipboard-read' });
-
+  
   if (permission.state === "denied") {
     throw new Error("Not allowed to read clipboard.");
   }
@@ -16,14 +15,27 @@ try {
 
   navigator.clipboard.readText()
     .then(textInClipboard => {
-      localStorage.setItem("venueObject", textInClipboard);
 
-      let venueObject = JSON.parse(localStorage.getItem("venueObject"));
+      const googleMapLink = {};
+      let venueObject = {};
+
+      if (textInClipboard.indexOf("https://goo.gl/maps/" === 0)) {
+        googleMapLink.googleMapLink = textInClipboard;
+        // localStorage.setItem("googleMapLink", textInClipboard);
+      } else if (textInClipboard.indexOf("{") === 0) {
+        venueObject = JSON.parse(textInClipboard);
+        // localStorage.setItem("venueObject", textInClipboard);
+      }
+
+      // const venueObject = JSON.parse(localStorage.getItem("venueObject"));
+      // const googleMapLink = JSON.parse(localStorage.getItem("googleMapLink"));
       console.log(venueObject);
+      console.log(googleMapLink);
       ///////////////////////////////////////////////////////////////////////////
       const jiraFields = document.querySelectorAll(".gbXyYv");
       console.log(jiraFields);
       for (let i = 0; i < jiraFields.length; i++) {
+
         if (jiraFields[i].innerText.includes("Category")) {
 
           let n = venueObject["category:pl"];
@@ -66,49 +78,10 @@ try {
 
         }
       //////////////////////////////////////////////////////////////////////////
-      if (jiraFields[i].innerText.includes("Entity Name")) {
-        let n = venueObject["name"];
-        console.log("!!!!!!!");
-        console.log(venueObject);
-        console.log(n);
-        console.log("!!!!!!!");
-
-        let eventForOnChange = {
-          target: {
-            value: n,
-            // validationMessage: "invalid..."
-          }
-        };
-
-
-        let jiraFieldValueDiv = jiraFields[i].querySelector(".kLiHRY");
-        jiraFieldValueDiv.click();
-
-        let jiraFieldValueInput = document.querySelectorAll(".bfCuIo");
-        jiraFieldValueInput[0].focus();
-        jiraFieldValueInput[0].click();
-        console.log(jiraFieldValueInput);
-
-        const inputKeys = Object.keys(jiraFieldValueInput[0]);
-        console.log(inputKeys);
-
-        for (let i = 0; i < inputKeys.length; i++) {
-          if (inputKeys[i].includes("reactEventHandler")) {
-            console.log("@@@@@");
-            console.log(inputKeys[i]);
-            console.log("@@@@@");
-            jiraFieldValueInput[0][inputKeys[i]].onChange(eventForOnChange);
-          } else {
-            console.log("Error: reactEventHandler not found");
-          }
-        }
-        let hui = document.querySelector(".iNYbiH");
-        hui.click();
 
       }
-
-
-      }
+      // localStorage.removeItem("venueObject");
+      // localStorage.removeItem("googleMapLink");
       //////////////////////////////////////////////////////////////////////////
     })
     .catch((error) => {
@@ -122,6 +95,6 @@ try {
 }
 
 
-function pasteToJira() {
+// function pasteToJira() {
 
-}
+// }
--- Zapolnyator/popup/popup.js
+++ Zapolnyator/popup/popup.js
@@ -1,5 +1,6 @@
 const copyBtn = document.getElementById("copyButton");
 const pasteBtn = document.getElementById("pasteButton");
+const helpButton = document.getElementById("helpButton");
 
 copyBtn.addEventListener("click", async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
@@ -18,62 +19,120 @@ pasteBtn.addEventListener("click", async () => {
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     function: startScript
-    // files: ['/popup/snowfall2020.js']
+    // files: ['/popup/pasteToJira.js']
   });
 
   window.close();
 });
 
-const copyFromEditor = async () => {
+helpButton.addEventListener("click", () => {
+  alert(`
+  1. Select Point in Venues Editor
+     ('http://venues.placer.team:8080/...' or
+     'http://staging-venues.placer.team:8080/...') or
+     Google Maps ('https://www.google.com/maps/...').
+  2. Copy the data by clicking "Copy".
+  3. Switch to Jira ('https://placer.atlassian.net/...').
+  4. Paste the data to Jira by clicking "Paste".
+     _____________________________________________
+     You must give browser permissions for "Clipboard";
+  `);
+});
+
+const copyFromEditor = () => {
   try {
-    if (window.location.hostname.indexOf("venues.placer") === -1) {
-      throw new Error("Switch to Venue Editor\n('http://venues.placer.team:8080/...' or\n'http://staging-venues.placer.team:8080/...')");
+    const currentURL = window.location.href;
+    const venueObject = {};
+
+    //// copy from Venue Editor
+    if (currentURL.indexOf("http://venues.placer.") === 0 || currentURL.indexOf("http://staging-venues.placer.") === 0) {
+
+      const venueObjKey = document.querySelectorAll(".tag-list .key-wrap>input");
+      const venueObjValue = document.querySelectorAll(".tag-list .value-wrap>input");
+
+      for (let i = 0; i < venueObjKey.length; i++) {
+
+        if (([venueObjKey[i].value] == "name" && !venueObjValue[0].value) ||
+          ([venueObjKey[i].value] == "addr:city" && !venueObjValue[i].value) ||
+          ([venueObjKey[i].value] == "addr:postcode" && !venueObjValue[i].value) ||
+          ([venueObjKey[i].value] == "addr:state" && !venueObjValue[i].value) ||
+          ([venueObjKey[i].value] == "addr:street" && !venueObjValue[i].value) ||
+          ([venueObjKey[i].value] == "category:pl" && !venueObjValue[i].value) ||
+          ([venueObjKey[i].value] == "opening_hours:pl" && !venueObjValue[i].value) ||
+          ([venueObjKey[i].value] == "manually_reviewed_status:pl" && !venueObjValue[i].value)) {
+            throw new Error("Value in required field is empty");
+        }
+        venueObject[venueObjKey[i].value] = venueObjValue[i].value;
+      };
+
+      const zoomToSelectionBtn = document.querySelector(".zoom-to-selection-control>.disabled");
+
+      if (!venueObject["oid:pl"] || zoomToSelectionBtn !== null) {
+        throw new Error("Pick a point first");
+      }
+
+      //// check isProperty exist and has value in venueObject
+      if (!venueObject.hasOwnProperty("name") || !venueObject.hasOwnProperty("addr:city") || !venueObject.hasOwnProperty("addr:postcode") ||
+        !venueObject.hasOwnProperty("addr:state") || !venueObject.hasOwnProperty("addr:street") || !venueObject.hasOwnProperty("category:pl") || 
+        !venueObject.hasOwnProperty("opening_hours:pl") || !venueObject.hasOwnProperty("manually_reviewed_status:pl") ) {
+          throw new Error("Add required Venue property");
+      }
+
+      //// add properties to venueObject
+      venueObject.fullAddress = `${venueObject["addr:street"]}, ${venueObject["addr:city"]}, ${venueObject["addr:state"]} ${venueObject["addr:postcode"]}`;
+      venueObject.analyticsUrlCutted = venueObject["analytics:url:pl"].substring(0, venueObject["analytics:url:pl"].indexOf("?"));
+      venueObject.venueURL = currentURL;
+
+      //// copy from Google Maps
+    } else if (currentURL.indexOf("https://www.google.com/maps/") === 0) {
+
+      if (currentURL.indexOf("https://www.google.com/maps/place/") === -1) {
+        throw new Error("Pick a point first");
+      }
+
+      //// copy data to Clipboard
+      const shareBtn = document.querySelector("button[data-value='Share']");
+      shareBtn.click();
+      shareBtn.click();
+      console.dir(shareBtn);
+
+      const shareBtnLinkInput = this.find("#modal-dialog");
+      console.dir(this);
+      console.dir(shareBtnLinkInput);
+      // venueObject.googleMapLink = shareBtnLinkInput.value;
+
+    } else {
+      throw new Error("Switch to Venue Editor\n('http://venues.placer.team:8080/...' or\n'http://staging-venues.placer.team:8080/...') or\nGoogle Maps ('https://www.google.com/maps/...')");
     }
 
-    const venueObjKey = document.querySelectorAll(".tag-list .key-wrap>input");
-    const venueObjValue = document.querySelectorAll(".tag-list .value-wrap>input");
-
-    let venueObject = {};
-
-    for (let i = 0; i < venueObjKey.length; i++) {
-      venueObject[venueObjKey[i].value] = venueObjValue[i].value;
-    };
-
-    const zoomToSelectionBtn = document.querySelector(".zoom-to-selection-control>.disabled");
-
-    if (!venueObject["oid:pl"] || zoomToSelectionBtn !== null) {
-      throw new Error("First choose Placer Venue");
-    }
-
-    // console.log(venueObject);
+    //// Copy venueObject to Clipboard
     const bufferDivCopy = document.createElement("textarea");
     bufferDivCopy.id = "bufferDivCopy";
     bufferDivCopy.style.cssText = `
     position: absolute;
-    height: 300px;
-    width: 200px;
     top: -99999px;
     left: -99999px;
     z-index: -99999;
     opacity: 0;
     `;
-    const venueObjectJSON = JSON.stringify(venueObject);
-    bufferDivCopy.innerHTML = venueObjectJSON;
+
+    bufferDivCopy.innerHTML = JSON.stringify(venueObject);
     document.body.append(bufferDivCopy);
     bufferDivCopy.select();
     document.execCommand("copy");
     bufferDivCopy.remove();
+
   } catch (error) {
     alert(`${error}\nCopy failed!`);
     console.dir(error);
   }
 }
 
-const startScript = async () => {
-  const s = document.createElement('script');
-  s.src = chrome.runtime.getURL('/popup/snowfall2020.js');
-  s.onload = function () {
+const startScript = () => {
+  const contextScript = document.createElement('script');
+  contextScript.src = chrome.runtime.getURL('/popup/pasteToJira.js');
+  contextScript.onload = function () {
     this.remove();
   };
-  (document.head || document.documentElement).append(s);
+  (document.head || document.documentElement).append(contextScript);
 };
