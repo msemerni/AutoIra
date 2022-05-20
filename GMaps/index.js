4b2b9828e4aaad6086233a0ba8afff6c3eb4ad30
msemerni <you@example.com>
2022-05-21 00:06:41 +0300

    gmaps

--- GMaps/index.js
+++ GMaps/index.js
@@ -29,42 +29,42 @@ let valueArray = [];
 // // value = hoursTextObj[key]; // значение ключа key
 
 
-if (hoursTextObj.hasOwnProperty("mo")) {
-  keysArray.push("mo");
-  valueArray.push(hoursTextObj["mo"]);
-}
-if (hoursTextObj.hasOwnProperty("tu")) {
-  keysArray.push("tu");
-  valueArray.push(hoursTextObj["tu"]);
-}
-if (hoursTextObj.hasOwnProperty("we")) {
-  keysArray.push("we");
-  valueArray.push(hoursTextObj["we"]);
-}
-if (hoursTextObj.hasOwnProperty("th")) {
-  keysArray.push("th");
-  valueArray.push(hoursTextObj["th"]);
-}
-if (hoursTextObj.hasOwnProperty("fr")) {
-  keysArray.push("fr");
-  valueArray.push(hoursTextObj["fr"]);
-}
-if (hoursTextObj.hasOwnProperty("sa")) {
-  keysArray.push("sa");
-  valueArray.push(hoursTextObj["sa"]);
-}
-if (hoursTextObj.hasOwnProperty("su")) {
-  keysArray.push("su");
-  valueArray.push(hoursTextObj["su"]);
-}
+// if (hoursTextObj.hasOwnProperty("mo")) {
+//   keysArray.push("mo");
+//   valueArray.push(hoursTextObj["mo"]);
+// }
+// if (hoursTextObj.hasOwnProperty("tu")) {
+//   keysArray.push("tu");
+//   valueArray.push(hoursTextObj["tu"]);
+// }
+// if (hoursTextObj.hasOwnProperty("we")) {
+//   keysArray.push("we");
+//   valueArray.push(hoursTextObj["we"]);
+// }
+// if (hoursTextObj.hasOwnProperty("th")) {
+//   keysArray.push("th");
+//   valueArray.push(hoursTextObj["th"]);
+// }
+// if (hoursTextObj.hasOwnProperty("fr")) {
+//   keysArray.push("fr");
+//   valueArray.push(hoursTextObj["fr"]);
+// }
+// if (hoursTextObj.hasOwnProperty("sa")) {
+//   keysArray.push("sa");
+//   valueArray.push(hoursTextObj["sa"]);
+// }
+// if (hoursTextObj.hasOwnProperty("su")) {
+//   keysArray.push("su");
+//   valueArray.push(hoursTextObj["su"]);
+// }
 
-let result = `${keysArray[0]}`;
-for (let i = 0; i < valueArray.length; i++) {
-  if (valueArray[i] === valueArray[i+1]) {
-    result += `${keysArray[i+1]}`
-  } 
+// let result = `${keysArray[0]}`;
+// for (let i = 0; i < valueArray.length; i++) {
+//   if (valueArray[i] === valueArray[i+1]) {
+//     result += `${keysArray[i+1]}`
+//   } 
 
-}
+// }
 
 
 console.log(result);
@@ -86,6 +86,8 @@ for (const key in hoursTextObj) {
   /////////////////////////////////////////////////////////
   // https://g.page/woodchucksfurniture?share
   // https://goo.gl/maps/1AHE6PgRJFHMs6qf9
+  // https://g.page/fblinen?share
+  // https://goo.gl/maps/4tAEVykgZ5P8nCko9
 
 
 
@@ -93,4 +95,7 @@ for (const key in hoursTextObj) {
     this.blur();
     var r=[],o=[];
     var e=utilOpeningHoursToString(utilFixTimeOverlapping(p.filter(function(t,e){var n=" (Row ".concat(e+1,")");
-    return!!(t.day||t.time.start||t.time.end)&&utilIsValidOperatingHourData(t,function(e){r.push(e+n),f.add(t.id)},function(e){o.push(e+n),g.add(t.id)})}),function(e){r.push(e)})),t={"opening_hours:pl":e};o.length||r.length?(s.container().call(n,o,r,e,L),S()):L(t)}
+    return!!(t.day||t.time.start||t.time.end)&&utilIsValidOperatingHourData(t,function(e){r.push(e+n),f.add(t.id)},function(e){o.push(e+n),g.add(t.id)})}),function(e){r.push(e)})),t={"opening_hours:pl":e};
+    o.length||r.length?(s.container().call(n,o,r,e,L),S()):L(t)}
+
+    
